78ba8c6c828012980bc2f4006d35ebdb2d4a6a52
Scott Dyer <sadyer@ucdavis.edu>
2011-07-08 11:40:17 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DssEvote::Application.config.secret_token = 'bb0dea8219d1454317a0ba22f51e59fa0ed06bfef257dd77231f0ab2449b358448d4307abf28f350d2150e67a83a5bb1adc1d3f0205afd979ef4e9b7d5543272'
